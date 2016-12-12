fbaac24f5967e83286337f4d8f22efbd88b2c37f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-12 13:12:14 +0000

    Ball can destroy blocks

---
+++ Assets/scripts/brick/Brick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	private void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "ball") {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/scripts/brick/BrickBroken.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BrickBroken : MonoBehaviour {
+
+	[SerializeField]
+	private Sprite damagedBrickSprite;
+
+	private SpriteRenderer sprRen;
+	private int health = 2;
+
+	private void Awake () {
+		sprRen = GetComponent<SpriteRenderer>();
+	}
+
+	private void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "ball") {
+			TakeDamage();
+		}
+	}
+
+	private void TakeDamage () {
+		health--;
+		if (health <= 0) {
+			Destroy(gameObject);
+		} else {
+			sprRen.sprite = damagedBrickSprite;
+		}
+	}
+}
