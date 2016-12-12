f8b5233e3c339ca4675b376448c769c8eaabce9c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-12 13:04:29 +0000

    Ball bounces off the racket at different directions and is destroy when it falls of the board

---
+++ Assets/scripts/ball/Ball.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ball : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private Rigidbody2D rb;
+
+	private void Awake () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start () {
+		rb.velocity = Vector2.up * moveSpeed;
+	}
+
+	private float CalculateBounceDirection (Vector2 ballPosition, Vector2 racketPosition) {
+		return (ballPosition.x - racketPosition.x);
+	}
+
+	private void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "racket") {
+			float x = CalculateBounceDirection(transform.position, other.transform.position);
+			Vector2 direction = new Vector2(x, 1).normalized; // TODO - Use trig to set y value to ensure constant speed of ballPosition
+			rb.velocity = direction * moveSpeed;
+		}
+	}
+
+	private void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "bottom") {
+			Debug.Log("Game Over");
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/scripts/racket/RacketController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class RacketController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private void FixedUpdate () {
+		float h = Input.GetAxisRaw("Horizontal");
+
+		if (h > 0) {
+			MoveRight(true, moveSpeed);
+		}
+		if (h < 0) {
+			MoveRight(false, moveSpeed);
+		}
+	}
+
+	private void MoveRight(bool isMovingRight, float moveSpeed) {
+		Vector3 temp = transform.position;
+		temp.x += (isMovingRight) ? moveSpeed * Time.deltaTime : -moveSpeed * Time.deltaTime;
+		transform.position = temp;
+	}
+}
